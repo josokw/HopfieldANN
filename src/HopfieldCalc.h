07f44d1c2398be275dfffa058ed6c89da525082f
jos <jos.onokiewicz@han.nl>
2018-07-11 20:55:50 +0200

    Updated layout

--- src/HopfieldCalc.h
+++ src/HopfieldCalc.h
@@ -1,33 +1,30 @@
-/*---------------------------------------------------------------------------*/
-/* Simulation Hopfield ANN                                                   */
-/*---------------------------------------------------------------------------*/
-
 #ifndef HOPFIELDCALC_H
 #define HOPFIELDCALC_H
 
 #include "Hopfield.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-void LearnJ(int MaxPat, int PatSize, double J[][MAXN]);
+    void LearnJ(int MaxPat, int PatSize, double J[][MAXN]);
 
-int AddNoise(int PatSize, int PatNumber, double Pat[], int Chance);
+    int AddNoise(int PatSize, int PatNumber, double Pat[], int Chance);
 
-void CalcOut(int PatSize, const double J[][MAXN], const double InPattern[],
-             double OutPattern[]);
+    void CalcOut(int PatSize, const double J[][MAXN], const double InPattern[],
+                 double OutPattern[]);
 
-void CopyPattern(int PatSize, const double sourcePattern[],
-                 double targetPattern[]);
+    void CopyPattern(int PatSize, const double sourcePattern[],
+                     double targetPattern[]);
 
-double CalcEnergy(int PatSize, const double Pattern[], const double J[][MAXN]);
+    double CalcEnergy(int PatSize, const double Pattern[], const double J[][MAXN]);
 
-void CalcAssociations(int Patsize,
-                      const double J[][MAXN],
-                      const double InputPattern[],
-                      const double InputPatternWithNoise[],
-                      double AssociationPattern[]);
+    void CalcAssociations(int Patsize,
+                          const double J[][MAXN],
+                          const double InputPattern[],
+                          const double InputPatternWithNoise[],
+                          double AssociationPattern[]);
 
 #ifdef __cplusplus
 }
